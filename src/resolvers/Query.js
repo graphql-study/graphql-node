64d1f4216983078d39840cc7dc61070cc751887b
minkj1992 <minkj1992@gmail.com>
2021-03-06 21:27:44 +0900

    feat: query filter 추가

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,12 @@
 /*
+ * mongoose와 마찬가지로 prisma 또한 한국어 형태소 쿼리가 불가능하다. (100% 매칭되어야함)
+ * 하, 한국어 (x)
+ * ㅎ, 한국어 (x)
+ * 한, 한국어 (o)
+ * 한국, 한국어 (o)
+ 
 {
-  feed {
+  feed(filter: "한국어 테스트") {
     id
     url
     description
@@ -8,7 +14,15 @@
 }
 */
 async function feed(parent, args, context, info) {
-  return await context.prisma.link.findMany();
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+  return await context.prisma.link.findMany({ where });
 }
 
 module.exports = {
